db40eb219aafb420c879ee3daca18942eeb05a7d
OuyangWei <30515693+ayowin@users.noreply.github.com>
2026-04-15 03:51:33 +0800

    Update BACKEND_API to use placeholder for IP

--- frontend/app.js
+++ frontend/app.js
@@ -6,7 +6,7 @@
 
 const CONFIG = {
     // BACKEND_API需要配置后端服务器ip
-    BACKEND_API: 'http://192.168.0.7:5000/api',
+    BACKEND_API: 'http://[后端服务器ip]:5000/api',
     // OSAPI为127.0.0.1即可
     OSAPI: 'http://127.0.0.1:8888/api'
 };
